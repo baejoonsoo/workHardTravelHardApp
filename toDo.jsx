a2c6a00db12a2a98844d089b79ad3fde6d754b73
baejoonsoo <baejoonsoo21@gmail.com>
2022-01-18 01:55:21 +0900

    Feat: patch toDo text

--- toDo.jsx
+++ toDo.jsx
@@ -1,8 +1,22 @@
-import { Alert, StyleSheet, TouchableOpacity, View, Text } from "react-native";
-import { Fontisto, MaterialCommunityIcons } from "@expo/vector-icons";
+import {
+  Alert,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+  TextInput,
+} from "react-native";
+import {
+  Fontisto,
+  MaterialCommunityIcons,
+  FontAwesome,
+} from "@expo/vector-icons";
 import { useState } from "react";
 
 const ToDos = ({ toDos, saveToDos, setToDos, k }) => {
+  const [patching, setPatching] = useState(false);
+  const [text, setText] = useState(toDos[k].text);
+
   const deleteToDo = (key) => {
     Alert.alert("정말로 삭제하시겠습니까?", "이 행동은 되돌릴 수 없습니다", [
       {
@@ -36,6 +50,31 @@ const ToDos = ({ toDos, saveToDos, setToDos, k }) => {
     setToDos(newToDo);
   };
 
+  const modeChange = () => {
+    if (!patching) {
+      setPatching(true);
+    } else {
+      onPatching();
+    }
+  };
+
+  const changeToDoText = (payload) => {
+    setText(payload);
+  };
+
+  const onPatching = () => {
+    const clickedToDo = toDos[k];
+    const newToDo = {
+      ...toDos,
+    };
+    newToDo[k] = {
+      ...clickedToDo,
+      text: text,
+    };
+    setToDos(newToDo);
+    setPatching(false);
+  };
+
   return (
     <View style={styles.toDo}>
       <View style={styles.toDoBox}>
@@ -54,18 +93,40 @@ const ToDos = ({ toDos, saveToDos, setToDos, k }) => {
             color="white"
           />
         )}
-        <Text
-          style={{
-            ...styles.toDoText,
-            color: toDos[k].checked ? "grey" : "white",
-          }}
-        >
-          {toDos[k].text}
-        </Text>
+        {patching ? (
+          <TextInput
+            style={styles.textPatch}
+            value={text}
+            onSubmitEditing={onPatching}
+            autoCapitalize="none"
+            returnKeyType="done"
+            onChangeText={changeToDoText}
+          />
+        ) : (
+          <Text
+            style={{
+              ...styles.toDoText,
+              color: toDos[k].checked ? "grey" : "white",
+            }}
+          >
+            {toDos[k].text}
+          </Text>
+        )}
       </View>
-      <TouchableOpacity onPress={() => deleteToDo(k)}>
-        <Fontisto name="trash" size={18} color="white"></Fontisto>
-      </TouchableOpacity>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "flex-end",
+          flex: 1,
+        }}
+      >
+        <TouchableOpacity style={{ marginRight: 10 }} onPress={modeChange}>
+          <FontAwesome name="pencil" size={20} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => deleteToDo(k)}>
+          <Fontisto name="trash" size={18} color="white"></Fontisto>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -80,17 +141,30 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
+    flex: 1,
   },
   toDoBox: {
     flexDirection: "row",
     alignItems: "center",
+    flex: 5,
+  },
+  textPatch: {
+    borderBottomColor: "#f4f4f4",
+    borderBottomWidth: 1,
+    marginLeft: 10,
+    borderRadius: 10,
+    paddingLeft: 10,
+    fontSize: 16,
+    color: "#f4f4f4",
+    paddingBottom: 3,
+    flex: 0.95,
   },
   toDoText: {
     paddingLeft: 10,
     fontSize: 16,
     fontWeight: "500",
     overflow: "hidden",
-    flex: 0.95,
+    flex: 0.98,
   },
 });
 
